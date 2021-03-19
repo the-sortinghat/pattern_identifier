a9a756d17fcfb2f20fb19dc26258cc9fe3cf94c5
João Daniel <jotaf.daniel@gmail.com>
2021-03-19 19:02:56 -0300

    set main to run all systems
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,24 @@ import { neo4jAdapter } from './neo4j';
 import { sharedDB } from './shared_db';
 import { CQRS } from './cqrs';
 
-async function main(): Promise<void> {
+async function analyze(system): Promise<void> {
   const { session, driver } = neo4jAdapter.connect();
 
-  const system = 'Hacknizer';
-
+  console.log('=======================\n');
   console.log(`Data relative to system "${system}"`);
 
   await sharedDB.run(session, system);
   await CQRS.run(session, system);
 
+  console.log('\n\n\n=======================');
+
   neo4jAdapter.disconnect(session, driver);
+}
+
+async function main(): Promise<void> {
+  await analyze('Hacknizer');
+  await analyze('Pingr');
+  await analyze('Fake');
 }
 
 main();
