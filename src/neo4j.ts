9cc766409b384bba29dd2e2c5a377d210c09d8e8
João Daniel <jotaf.daniel@gmail.com>
2021-03-19 16:58:41 -0300

    move neo4j credentials to .env
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/neo4j.ts
+++ src/neo4j.ts
@@ -1,14 +1,21 @@
+import { config } from 'dotenv';
 import neo4j from 'neo4j-driver';
 
+config();
+
 function connectNeo4j(): any {
-  const driver = neo4j.driver('bolt://3.83.214.196:7687', neo4j.auth.basic('neo4j', 'gate-honk-evaluations'), {});
+  const url = process.env.NEO4J_URL;
+  const user = process.env.NEO4j_USER;
+  const pw = process.env.NEO4J_PASSWORD;
+
+  const driver = neo4j.driver(url, neo4j.auth.basic(user, pw), {});
 
   const session = driver.session({ database: 'neo4j' });
 
   return { driver, session };
 }
 
-function disconnectNeo4j(session, driver) {
+function disconnectNeo4j(session, driver): void {
   session.close();
   driver.close();
 }
